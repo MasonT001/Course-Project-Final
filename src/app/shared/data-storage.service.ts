a2e546137ae1aa868ba1893339db90ff48bc2873
MasonT001 <mltrepagnier@gmail.com>
2023-02-03 11:16:47 -0800

    fetching recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -4,6 +4,7 @@ import { Recipe } from '../recipes/recipe.model'
 import { RecipeService } from '../recipes/recipe.service'
 
 
+
 @Injectable({providedIn: 'root'})
 export class DataStorageService { 
     constructor(private http: HttpClient, private recipeService: RecipeService) {}
@@ -13,6 +14,13 @@ export class DataStorageService {
         this.http.put('https://ng-course-recipe-book-32624-default-rtdb.firebaseio.com/recipes.json', recipes)
         .subscribe(response => {
             console.log(response)
+        })
+    }
+
+    fetchRecipes() {
+        this.http.get<Recipe[]>('https://ng-course-recipe-book-32624-default-rtdb.firebaseio.com/recipes.json')
+        .subscribe(recipes => {
+            this.recipeService.setRecipes(recipes)
         })
     }
 }
